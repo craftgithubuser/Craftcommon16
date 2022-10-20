d8c4264ef4dbefc57bcc97b193cf1679553a1d84
Mike <mikiaslema74@gmail.com>
2022-10-20 19:49:44 -0400

    commit

---
+++ javaMike/src/javaMike/MikeP1.java
@@ -0,0 +1,11 @@
+package javaMike;
+
+public class MikeP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Print something");
+
+	}
+
+}
---
+++ javaMike/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mikia
+ *
+ */
+module javaMike {
+}
