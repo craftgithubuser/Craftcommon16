1156b2d3d2c6d668a44af626411c3b9417070318
craftNunuye <114204071+craftNunuye@users.noreply.github.com>
2022-10-21 16:13:56 -0400

    second commit

--- SimpleSampleProject/src/com/craft/HelloClassTwo.java
+++ SimpleSampleProject/src/com/craft/HelloClassTwo.java
@@ -5,6 +5,8 @@ public class HelloClassTwo {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("hi again");
+		System.out.println("hi again");
+		System.out.println("hi again");
 		
 	}
 
