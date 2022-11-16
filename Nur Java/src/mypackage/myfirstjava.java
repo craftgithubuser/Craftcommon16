539e05d3f08e59ce9615419c271caccc6ce27a32
Nurmoh <nurmoh@gmail.com>
2022-11-16 18:28:35 -0500

    my first scanner commit

---
+++ Nur Java/src/mypackage/myfirstjava.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class myfirstjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int num;
+num=2;
+System.out.println(num);
+		
+		
+		
+	}
+
+}
