718520accad036b82649d0ff6d5f38b0b57ebac1
nardosmekonen2010@gmail.com <115605276+abay1234art@users.noreply.github.com>
2022-11-16 16:13:27 -0600

    commit Scanner eclipse

--- common_16/src/com_15/StudentInfo.java
+++ common_16/src/com_15/StudentInfo.java
@@ -7,7 +7,6 @@ public class StudentInfo {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println();
 		
 		 System.out.println("pls enter your full name:");
 		 String StudentInfoName = input.next();
