01e80950a73dd95c90046556a107431b543362da
tes-77 <zemen1990@gmail.com>
2022-11-17 14:26:36 -0500

    hallo world people

---
+++ craft.common.13/src/com/day2/EmployeeInfo.java
@@ -0,0 +1,20 @@
+package com.day2;
+
+public class EmployeeInfo {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+String empFirstName="James";
+String empLastName="Fox"; 
+int empAge=45;
+double empSalary=2345.33;
+double empBonus = 301;
+double totalSalary = empSalary +empBonus; 
+System.out.println(empFirstName);
+System.out.println(empLastName);
+System.out.println(empAge);
+System.out.println(empSalary);
+System.out.println(empBonus);}
+
+}
