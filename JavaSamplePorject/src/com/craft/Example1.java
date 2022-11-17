266b87a94ddb04b77e47be1eef25ddf287663218
Gebrehiwot2022 <gebrehiwotganfure@gmail.com>
2022-11-16 23:11:09 -0500

    Commit from eclipse

---
+++ JavaSamplePorject/src/com/craft/Example1.java
@@ -0,0 +1,11 @@
+package com.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("What challnging is to push your work to Github");
+	}
+
+}
