6f42b238f81240e3b2fdc23420b62614627c6e71
Michael, Melkamu <sisaymelkamu@gmail.com>
2022-11-17 06:52:19 -0500

    more editing work on HelloWorldApp is made.

--- HelloWorld/src/com/helloworld/HelloWorldApp.java
+++ HelloWorld/src/com/helloworld/HelloWorldApp.java
@@ -4,15 +4,14 @@ public class HelloWorldApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("Hello Everybody!!!");
-     System.out.println("Hello Everybody!!!");
-	 System.out.println("Hello Everybody!!!");
-	 System.out.println("Hello Everybody!!!");
-	 System.out.println("Hello Everybody!!!");
-	 System.out.println("Hello Everybody!!!");
-	 System.out.println("Hello Everybody!!!");
-	 System.out.println("Hello Everybody!!!");
-	
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!!");
 
 	}
 
