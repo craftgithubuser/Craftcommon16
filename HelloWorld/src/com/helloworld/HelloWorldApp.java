6fefcea7252e4276a962a1df8293750c4a28160a
Michael, Melkamu <sisaymelkamu@gmail.com>
2022-11-17 06:37:51 -0500

    HelloWorldApp.java code is committed.

---
+++ HelloWorld/src/com/helloworld/HelloWorldApp.java
@@ -0,0 +1,19 @@
+package com.helloworld;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello Everybody!!!");
+     System.out.println("Hello Everybody!!!");
+	System.out.println("Hello Everybody!!!");
+	System.out.println("Hello Everybody!!!");
+	System.out.println("Hello Everybody!!!");
+	System.out.println("Hello Everybody!!!");
+	System.out.println("Hello Everybody!!!");
+	System.out.println("Hello Everybody!!!");
+	
+
+	}
+
+}
