d24a9a395419c1abbb2703e0886f37b64c661ff2
gashti03 <115651101+gashti03@users.noreply.github.com>
2022-11-17 10:10:35 -0800

    GashawKassahun3 commit

--- com.javapractice/src/com/practiceday1/employeeinfo2.java
+++ com.javapractice/src/com/practiceday1/employeeinfo2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class employeeinfo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// more additions
 		
 		Scanner g = new Scanner (System.in);
 		
