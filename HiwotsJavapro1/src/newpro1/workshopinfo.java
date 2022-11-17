0aa628c25d599a5595f312d729e47bdd5f442743
hiwiwolde <h.woldetsadik@gmail.com>
2022-11-17 10:25:48 -0500

    another commit from eclipse

---
+++ HiwotsJavapro1/src/newpro1/workshopinfo.java
@@ -0,0 +1,27 @@
+package newpro1;
+
+public class workshopinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		 String firstName= ("James");
+		 String lastName=("Bond");
+
+		    double salary = 2300.99;
+
+		    double bonus = 100;
+
+		    double total_salary = salary + bonus;
+
+		    System.out.println("the name of the employee is : " + firstName);
+		    System.out.println("the name of the employee is : " + lastName);
+		    System.out.println("the salary of the employee is : " + salary);
+		    System.out.println("the bonus of the employee is : " + bonus);
+		    System.out.println("The total salary of the employee is : " + total_salary);
+		
+		
+	}
+
+}
