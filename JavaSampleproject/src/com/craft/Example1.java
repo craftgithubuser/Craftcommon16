1498998caef3aa51e08840365c7bd431ef91a144
yemash <115667858+yemash@users.noreply.github.com>
2022-11-18 01:19:18 -0500

    yemisrach assefa my second commit

--- JavaSampleproject/src/com/craft/Example1.java
+++ JavaSampleproject/src/com/craft/Example1.java
@@ -3,9 +3,11 @@ package com.craft;
 public class Example1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	// TODO Auto-generated method stub
 		
 		System.out.print("print somthing");
+		
 		
 
 	}
