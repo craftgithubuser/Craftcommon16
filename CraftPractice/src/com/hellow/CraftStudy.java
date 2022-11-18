882d353a4a29fe3262fbe2a13a4ce84a70cc5cc5
Etse12 <etsegenetb24@gmail.com>
2022-11-18 15:56:42 -0500

    Our first commit

---
+++ CraftPractice/src/com/hellow/CraftStudy.java
@@ -0,0 +1,12 @@
+package com.hellow;
+
+public class CraftStudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hellow");
+		System.out.println("hellow world");
+	}
+
+}
