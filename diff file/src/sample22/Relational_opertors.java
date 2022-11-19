ad4ae406ebd22bacc70cb91c17d6e500175e7588
anteneh <112592567+tuna-11@users.noreply.github.com>
2022-11-19 12:37:34 -0700

    relationalop1

---
+++ diff file/src/sample22/Relational_opertors.java
@@ -0,0 +1,38 @@
+package sample22;
+
+public class Relational_opertors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//create variable
+		int d =24,r=10;
+		
+		//value of a and b
+		System.out.println("d is" + d + "r is" + r);
+		
+		// ==operator
+		System.out.println(d==r);
+		
+		//!=operator
+		System.out.println(d!=r);
+
+		//>operator
+				System.out.println(d>r);
+				
+		//<operator
+				System.out.println(d<r);
+				
+		//>=operator
+				System.out.println(d>=r);
+				
+		//<=operator
+				System.out.println(d<=r);
+				
+				
+				
+				
+				
+	}
+
+}
