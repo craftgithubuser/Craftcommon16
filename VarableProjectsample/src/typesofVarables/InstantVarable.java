9af85e1c743e857c352b4f26eb5ea6f0ddcffe31
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-19 22:13:48 -0500

    sample instant varable

---
+++ VarableProjectsample/src/typesofVarables/InstantVarable.java
@@ -0,0 +1,17 @@
+package typesofVarables;
+
+public class InstantVarable {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	String studentName="Muna";
+	int studentID=0254;
+	String studentDep="Accounting";
+	
+	System.out.println("student Name"+studentName);
+	System.out.println("student ID"+studentID);
+	System.out.println("student Department"+studentDep);
+
+}
+}
