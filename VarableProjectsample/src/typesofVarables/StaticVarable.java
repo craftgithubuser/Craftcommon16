b8c2f41946cbe78f22f8354e844936723511fab1
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-19 22:23:41 -0500

    Static varable sample

---
+++ VarableProjectsample/src/typesofVarables/StaticVarable.java
@@ -0,0 +1,18 @@
+package typesofVarables;
+
+public class StaticVarable {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	String studentName="Elda";
+	int studentID=0256;
+	String studentDep="RN";
+	
+	System.out.println("student Name"+studentName);
+	System.out.println("student ID"+studentID);
+	System.out.println("student Department"+studentDep);
+
+}
+}
