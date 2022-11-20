d1dd0e426275cf7b5bbf5ffb69650d5ab5beef76
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-19 23:20:46 -0500

    exception

--- VarableProjectsample/src/typesofVarables/exeption.java
+++ VarableProjectsample/src/typesofVarables/exeption.java
@@ -1,5 +1,20 @@
 package typesofVarables;
 
 public class exeption {
-
+	
+	public void methodOne() {
+		int J=50;
+		System.out.println("Value of J"+J);
+	}
+public void methodtwo( ) {
+	int K=60;
+			System.out.println("Value of K"+K);
+}
+   public static void main(String[] argus) {
+	   
+	   exeption m1= new exeption();
+	   m1.methodOne();
+	   m1.methodtwo();
+	   
+   }
 }
