71c2aff9a75d4018dc955b16fa35a5b5b6a32762
Hilina51 <115661921+Hilina51@users.noreply.github.com>
2022-11-20 09:09:03 +0000

    commit

--- eclipse-workspace/scannerproject/src/scannerproject/scannerproject1.java
+++ eclipse-workspace/scannerproject/src/scannerproject/scannerproject1.java
@@ -1,9 +1,47 @@
 package scannerproject;
 
+import java.util.Scanner;
+
 public class scannerproject1 {
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
+		
+		
+		Scanner hilina=	new Scanner(System.in);
+		
+		System.out.println("Pls enter your first name :");
+
+		String studentfirstName= hilina.next();
+
+		System.out.println("Pls enter your last name :");
+
+		String  StudentlastName= hilina.next();
+
+		System.out.println("Pls enter your age:");
+
+		int studentage= hilina.nextInt();
+
+		System.out.println("Pls enter your Salary");
+
+		double studentSalary=hilina.nextDouble();
+
+		System.out.println("Pls enter your Bonus");
+
+		double studentBonus=hilina.nextDouble();
+
+		System.out.println("Pls enter your total salary");
+
+		double studentTotalSalary=hilina.nextDouble();
+
+
+
+				
+
+			
+				}
+
 
 	}
 
