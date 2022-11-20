3af69134bb6cc6e787b42e16ff83e93c5b86b6ba
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-19 22:09:56 -0500

    update

---
+++ VarableProjectsample/src/typesofVarables/LocalVarable.java
@@ -0,0 +1,17 @@
+package typesofVarables;
+
+public class LocalVarable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String studentName="Hannna";
+		int studentID=0237;
+		String studentDep="IT";
+		
+		System.out.println("student Name"+studentName);
+		System.out.println("student ID"+studentID);
+		System.out.println("student Department"+studentDep);
+	}
+
+}
