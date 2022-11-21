1fd983e41445417500e78ad508c804733f58a4d5
anteneh <112592567+tuna-11@users.noreply.github.com>
2022-11-20 22:17:14 -0700

    unary operator

---
+++ diff file/src/Unary_oper.java
@@ -0,0 +1,15 @@
+
+public class Unary_oper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int b =10;
+		System.out.println(b++);
+		System.out.println(++b);
+		System.out.println(b--);
+		System.out.println(--b);
+
+	}
+
+}
