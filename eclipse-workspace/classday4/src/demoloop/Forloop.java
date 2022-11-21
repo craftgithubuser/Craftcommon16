3720746ea8869e58b06956506ac02249a030e597
Hilina51 <115661921+Hilina51@users.noreply.github.com>
2022-11-21 19:57:16 +0000

    for loop commit

---
+++ eclipse-workspace/classday4/src/demoloop/Forloop.java
@@ -0,0 +1,14 @@
+package demoloop;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 50; i++) {
+			if (i%2==0)
+			System.out.print(i);
+		}
+
+	}
+}
