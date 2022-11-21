a29befba919b3c6c21c574e7026a2970da2f90e9
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-20 20:38:32 -0500

    Arithmetic operation Sample

---
+++ VarableProjectsample/src/ArtimaticOperaionJava/ArtimaicoperationPractice.java
@@ -0,0 +1,28 @@
+package ArtimaticOperaionJava;
+
+public class ArtimaicoperationPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a=12;
+		b=2;
+		c=a+b;
+		System.out.println("addition:"+c);
+		c=a-b;
+		System.out.println("subtracion:"+c);
+		c=a*b;
+		System.out.println("division:"+c);
+		c=a/b;
+		System.out.println("division:"+c);
+		a=13;
+		c=a%b;
+		System.out.println("Remainders:"+c);
+		
+		
+		
+
+	}
+
+}
