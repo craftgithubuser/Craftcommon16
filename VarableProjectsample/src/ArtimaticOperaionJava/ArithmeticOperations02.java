27f4ea59f65371d7cfa8a79d6447e82f9896626c
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-20 23:19:02 -0500

    Boolean Practice

--- VarableProjectsample/src/ArtimaticOperaionJava/ArithmeticOperations02.java
+++ VarableProjectsample/src/ArtimaticOperaionJava/ArithmeticOperations02.java
@@ -4,7 +4,15 @@ public class ArithmeticOperations02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    
+		boolean a=true;
+		boolean b=false;
+		
+		System.out.println("a&&b="+(a&&b));
+		System.out.println("a||b="+(a||b));
+		System.out.println("!(a&&b)="+ !(a&b));
+		
+		
 	}
 
 }
