e42de8d4c36efd2d7ff25ee0a1cff58c7610db82
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-21 15:01:22 -0500

    update

--- VarableProjectsample/src/ArtimaticOperaionJava/ForLoop.java
+++ VarableProjectsample/src/ArtimaticOperaionJava/ForLoop.java
@@ -5,7 +5,7 @@ public class ForLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i=1;i<=10;i++) {
+		for(int i=2;i<=10;i++) {
 			System.out.println(i);
 		}
 	}
