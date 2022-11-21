ab4bc2e53dc114ea68965252a6a7eea4db84e991
gashti03 <115651101+gashti03@users.noreply.github.com>
2022-11-20 20:34:23 -0800

    gashawK commit

---
+++ practiceday1/src/com/day1pack/LocalVariablePractice.java
@@ -0,0 +1,34 @@
+package com.day1pack;
+
+public class LocalVariablePractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LocalVariablePractice g1 = new LocalVariablePractice(); // creating an object
+		
+		g1.method1();
+		g1.method2();
+		
+		
+	}
+	
+	public void method1() {
+		int j = 25; // local variable method1 
+		System.out.println("value of j:" + j);
+		
+	}
+		
+	
+
+	public void method2() {
+		
+		int k = 30; // local variable method2
+		System.out.println("value of k:" + k);
+		
+			
+	}
+
+
+	
+}
