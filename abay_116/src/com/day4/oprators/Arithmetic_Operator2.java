b78b12578b959562ce89a974ed3c2120e80eb467
nardosmekonen2010@gmail.com <115605276+abay1234art@users.noreply.github.com>
2022-11-22 00:35:49 -0600

    hello team

---
+++ abay_116/src/com/day4/oprators/Arithmetic_Operator2.java
@@ -0,0 +1,16 @@
+package com.day4.oprators;
+
+public class Arithmetic_Operator2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 10;
+		System.out.println(a++); //print = 10 a=11
+		System.out.println(++a); //print = 12 a =12
+		System.out.println(a--); //print = 12 a =11
+		System.out.println(--a); //print 10
+	}
+
+}
---
+++ abay_116/src/com/day4/oprators/Relational_Oprator2.java
@@ -0,0 +1,26 @@
+package com.day4.oprators;
+
+public class Relational_Oprator2 {
+
+	public static void main(String[] args) {
+	
+		
+		int a, b;
+		a=40;
+		b=40;
+		System.out.println("a == b = " + (a == b) ); //true
+		System.out.println("a != b = " + (a != b) ); //false
+		System.out.println("a > b = " + (a > b) ); //false
+		System.out.println("a < b = " + (a < b) ); //false
+		System.out.println("b >= a " + (b >= a) ); //true
+		System.out.println("b <= a " + (b <= a) ); //true
+
+	}
+
+}
+
+
+
+	
+
+
