7365ceea63a3c0f14e77163fed6bbc338c2b8f1c
Mtufa <metitesfaye@gmail.com>
2022-11-21 19:15:00 -0600

    Merge branch 'main' of https://github.com/craftgithubuser/Craftcommon16.git into main

--- Craftcommon16/craftcommon16/src/operation/Looplab.java
+++ Craftcommon16/craftcommon16/src/operation/Looplab.java
@@ -1,4 +1,28 @@
 
+package operation;
+
+public class Looplab{
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
+
+    // addition operator
+    System.out.println("a + b = " + (a + b));
+
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
   }
-}
+}      
+
+
 
