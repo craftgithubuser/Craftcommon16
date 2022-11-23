f08300eb88e979d6254f17c913d82bd8a35694c1
Yeshashwork <115598625+Yeshashwork@users.noreply.github.com>
2022-11-23 00:31:24 -0500

    push

---
+++ Craftcommon16/AdilJavaSampleProject/src/com/adilraft/Example1.java
@@ -0,0 +1,15 @@
+package com.adilraft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("print something");
+      System.out.println("print something");
+     
+      
+      
+	}
+	
+
+}
--- Craftcommon16/craftcommon16/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author metit
- *
- */
-module craftcommon16 {
-}
