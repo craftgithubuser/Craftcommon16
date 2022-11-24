7875773f6f75dd730a76f52e0f34b78684643b19
Yeshashwork <115598625+Yeshashwork@users.noreply.github.com>
2022-11-23 23:53:03 -0500

    arry

---
+++ ConditionAndBranching/src/com/If/Arraydemo.java
@@ -0,0 +1,24 @@
+package com.If;
+
+public class Arraydemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] Mark= {
+				{90,80,88,40},
+				{50,60,80,70},
+				{70,90,100,60}
+	
+		};
+		
+	for(int i = 0; i < 3; i++) {
+		int sum = 0; 
+		for(int j = 0; j < 4; j++);
+			sum += mark[i][j];
+		
+		
+		
+	}
+}
+
+}
