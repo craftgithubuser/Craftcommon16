a4133f060dee667c2c1a506b6164dcb87157e1a9
AberhamTiruneh <115605380+AberhamTiruneh@users.noreply.github.com>
2022-11-27 14:59:39 -0600

    Aberham's Array Assignment Challenge3

--- ReverseArrayChallenge/src/com/arrayReverse/ReveresArray.java
+++ ReverseArrayChallenge/src/com/arrayReverse/ReveresArray.java
@@ -7,10 +7,11 @@ public class ReveresArray {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-
+//This code allows the user to input array size
 		System.out.println("Please enter array size");
 		int arraySize = in.nextInt();
-
+		
+//This code allows the user to enter numbers
 		int[] getNum = new int[arraySize];
 		for (int i = 0; i < arraySize; i++) {
 			System.out.println("Enter element on index: " + i);
@@ -20,7 +21,7 @@ public class ReveresArray {
 		}
 		reverse(getNum);
 	}
-
+//This method reverses integer array
 	public static void reverse(int[] arr) {
 
 		System.out.println("Original Numbers:");
