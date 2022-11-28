b1ea404e9b46557881cb7594c9c7f54c1cc1e08b
Bemranran <115678039+Bemranran@users.noreply.github.com>
2022-11-27 19:10:18 -0500

    update

---
+++ Array/src/ArrayAray/ArrayDesending.java
@@ -0,0 +1,31 @@
+package ArrayAray;
+
+public class ArrayDesending {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age[]={62,25,32,82,22,52,73,100,102};
+		
+		int temp;
+		for(int i=0;i<age.length;i++) {
+			for(int j=i;j<age.length;j++) {
+				if(age [i]<age[j]) {
+					temp=age[i];
+					age[i]=age[j];
+					age[j]=temp;	
+					
+				}
+			}
+		}
+		for(int a: age) {
+			System.out.println(a);
+		
+	}
+	}
+}
+		
+		
+
+	
+
+
