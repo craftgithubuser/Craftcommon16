8c7a11710181c20092d4f3ee50467c4a59cd2468
Hilina51 <115661921+Hilina51@users.noreply.github.com>
2022-11-29 21:10:24 +0000

    test commit from eclipse

---
+++ intro_Java/src/com/oopsEncapsulation/Student.java
@@ -0,0 +1,25 @@
+package com.oopsEncapsulation;
+
+public class Student {
+	
+	private String firstname="Hilina";
+	private String lastname="Asnake";
+	private int age= 28;
+	private String college="Natural science";
+	public String getFirstname() {
+		return firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getCollege() {
+		return college;
+	}
+	
+	
+	
+
+}
