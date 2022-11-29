c9fc231a06022ad1c17dd564bd9602f4f314676e
gashti03 <115651101+gashti03@users.noreply.github.com>
2022-11-29 01:43:07 -0800

    learning commits

---
+++ practiceday6/src/com/day6pack/ReverseArrayChallenge.java
@@ -0,0 +1,35 @@
+package com.day6pack;
+
+import java.util.Scanner;
+
+public class ReverseArrayChallenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer[] intArray = {1,2,3,4,5};
+
+		  //print array starting from first element
+		    System.out.println("Original Array:");
+		    for(int i=0;i<intArray.length;i++)
+		         System.out.print(intArray[i] + "  ");
+
+		    System.out.println();
+
+		    //print array starting from last element
+		    System.out.println("Original Array printed in reverse order:");
+		         for(int i=intArray.length-1;i>=0;i--)
+	
+		      System.out.print(intArray[i] + "  ");
+		        
+		         
+		         
+	}
+	
+	void reverse() {}
+		
+		
+		
+	}
+
+
