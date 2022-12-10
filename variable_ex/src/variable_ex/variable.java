404b6bd6ee5b350512e24d17879008207cc6beef
yemash <115667858+yemash@users.noreply.github.com>
2022-12-09 22:00:58 -0500

    Yemisrach Assefa

---
+++ variable_ex/src/variable_ex/variable.java
@@ -0,0 +1,51 @@
+package variable_ex;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=24;
+		System.out.println("age=24");
+		
+		
+		  // byte type
+        byte b = 20;
+        System.out.println("b= " + b);
+    
+        // short type
+        short s = 20;
+        System.out.println("s= " + s);
+    
+        // int type
+        int i = 20;
+        System.out.println("i= " + i);
+    
+        // long type
+        long l = 22;
+        System.out.println("l= " + l);
+        
+        // float type
+        float f = 20.25f;
+        System.out.println("f= " + f);
+    
+        // double type
+        double d = 20.25;
+        System.out.println("d= " + d);
+        
+        // double type
+     
+        // float type
+       
+    
+        
+    
+        
+		
+		
+		
+	}
+
+	}
+
+
