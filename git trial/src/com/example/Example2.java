dab95601920b1d37b96a5de8def3b91f763b91d2
Zeaman <xeamank@gmail.com>
2023-05-07 19:09:13 -0400

    Example2 class is committed

---
+++ git trial/src/com/example/Example2.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello world!!!");
+     System.out.println("Hello world!!!");
+     System.out.println("Hello world!!!");
+	}
+	
+
+}
